5f103da3fae7d4ce49d0a946a7f819d4c382d055
Jake Soenneker <jake@soenneker.com>
2026-03-15 11:36:04 -0500

    CI work

--- test/Soenneker.Maui.Blazor.Bridge.Tests/Fixture.cs
+++ test/Soenneker.Maui.Blazor.Bridge.Tests/Fixture.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using Soenneker.Fixtures.Unit;
-using Soenneker.Maui.Blazor.Bridge.Registrars;
 using Soenneker.Utils.Test;
 
 namespace Soenneker.Maui.Blazor.Bridge.Tests;
@@ -25,7 +24,5 @@ public sealed class Fixture : UnitFixture
 
         IConfiguration config = TestUtil.BuildConfig();
         services.AddSingleton(config);
-
-        services.AddMauiBlazorBridgeAsScoped();
     }
 }
